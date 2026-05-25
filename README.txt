abbf0492699cb093937e03719b0395c1e0e8100e
jacksaragih18 <jacksastrawansaragih@gmail.com>
2026-05-26 01:29:36 +0700

    Upload SAGAR login app

---
+++ README.txt
@@ -0,0 +1,21 @@
+SAGAR INNOVATION - Firebase Auth Version
+
+Versi ini sudah berisi firebaseConfig yang dikirim user:
+apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, measurementId.
+
+Upload file berikut ke folder GitHub Pages:
+- index.html
+- manifest.json
+- sw.js
+- icon-192.png
+- icon-512.png
+
+Pastikan file ini juga sudah ada di folder yang sama:
+- backgroundsaja.jpeg
+
+Login:
+ID       : jackruth
+Password : 181025
+
+Catatan:
+Aplikasi ini memakai Firebase SDK melalui CDN/browser import, bukan npm. Karena itu kode import dari Firebase Console tidak ditempel 100% sama, tetapi konfigurasi firebaseConfig-nya sudah dimasukkan.
---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "sagar-firebase-auth-v3";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : null))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
